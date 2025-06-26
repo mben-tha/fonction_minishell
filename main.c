50d6e9048ac73596aadb1571b4b7f683913774ed
ben thaier <mehdi.benthaier@outlook.fr>
2025-06-26 13:47:49 +0200

    2eme

--- main.c
+++ main.c
@@ -2,18 +2,17 @@
 #include <stdlib.h>
 
 int len_clean_space(char *str);
+int is_double_operator(char *str);
 
 char    *clean_space(char *str)
 {
-    int     len;
     char    *res;
     int     i;
     int     j;
 
     j = 0;
     i = 0;
-    len = len_clean_space(str);
-    res = malloc(sizeof(char) * (len + 1));
+    res = malloc(sizeof(char) * (len_clean_space(str) + 1));
     if (!res)
         return (NULL);
     while (str[i] == ' ')
@@ -28,6 +27,16 @@ char    *clean_space(char *str)
                 break ;
             res[j++] = ' ';
         }
+		else if (is_double_operator(&str[i]))
+		{
+			if (i != 0 && str[i-1] != ' ')
+                res[j++] = ' ';
+			res[j++] = str[i];
+			res[j++] = str[i];
+			if (str[i+2] && str[i+2] != ' ')
+                res[j++] = ' ';
+			i += 2;
+		}
         else if (str[i] == '<' || str[i] == '>' || str[i] == '|')
         {
             if (i != 0 && str[i-1] != ' ')
@@ -43,7 +52,7 @@ char    *clean_space(char *str)
             i++;
         }
     }
-    res[i] = '\0';
+    res[j] = '\0';
     return res;
 }
 
@@ -66,15 +75,21 @@ int len_clean_space(char *str)
                 return len;
             len++;
         }
+		else if (is_double_operator(&str[i]))
+		{
+			if (i != 0 && str[i-1] != ' ')
+                len++;
+			if (str[i+2] && str[i+2] != ' ')
+                len++;
+			i += 2;
+			len += 2;
+		}
         else if (str[i] == '<' || str[i] == '>' || str[i] == '|')
         {
             if (i != 0 && str[i-1] != ' ')
-            {
                 len++;
-                
-            }
             if (str[i+1] && str[i+1] != ' ')
-                    len++;
+                len++;
             i++;
             len++;
         }
@@ -87,12 +102,18 @@ int len_clean_space(char *str)
     return len;
 }
 
+int is_double_operator(char *str)
+{
+    return ((str[0] == '<' && str[1] == '<') || (str[0] == '>' && str[1] == '>'));
+}
+
 int main(int ac, char **av)
 {
     char *test;
 
     test = clean_space(av[1]);
-    printf("%sY\n", test);
+    printf("[%s]\n", test);
+	free(test);
 
     // printf("%dY\n", len_clean_space(av[1]));
 }
